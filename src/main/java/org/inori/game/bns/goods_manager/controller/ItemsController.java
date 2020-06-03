c3f6bcfdc451fe3a62125ae9dce804179dcdc615
InoriHimea <icarus347@gmail.com>
2020-06-03 12:54:56 +0800

    增加ItemKey算号器

--- src/main/java/org/inori/game/bns/goods_manager/controller/ItemsController.java
+++ src/main/java/org/inori/game/bns/goods_manager/controller/ItemsController.java
@@ -1,12 +1,14 @@
 package org.inori.game.bns.goods_manager.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.inori.game.bns.goods_manager.entity.ItemsEntity;
 import org.inori.game.bns.goods_manager.model.SelectOption;
 import org.inori.game.bns.goods_manager.service.ItemsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -18,6 +20,15 @@ public class ItemsController {
 
     @Autowired
     private ItemsService itemsService;
+
+    /**
+     * 列出Items
+     * @return
+     */
+    @GetMapping("/list")
+    public Flux<List<ItemsEntity>> getItems() {
+        return null;
+    }
 
     /**
      * 获取Items为Select选择使用
--- src/main/java/org/inori/game/bns/goods_manager/util/GoodItemKeyGenerator.java
+++ src/main/java/org/inori/game/bns/goods_manager/util/GoodItemKeyGenerator.java
@@ -1,6 +1,18 @@
 package org.inori.game.bns.goods_manager.util;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public final class GoodItemKeyGenerator {
+
+    private static final char[] toBase64 = {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
+    };
 
     /**
      * 计算物品的Key
@@ -8,7 +20,23 @@ public final class GoodItemKeyGenerator {
      * @return
      */
     public static String generatorKey(int id) {
+        StringBuffer sb = new StringBuffer ( "AA" );
 
-        return null;
+        List<Character> list = new LinkedList<> (  );
+        int temp = id * 16;
+        while (temp >= 64) {
+            list.add ( toBase64[Math.floorMod ( temp, 64 )] );
+            temp = Math.floorDiv ( temp, 64 );
+        }
+        list.add ( toBase64[temp] );
+
+        Collections.reverse (list);
+        list.forEach ( character -> sb.append ( character ) );
+        return sb.append ( "==" ).toString ();
+    }
+
+    public static void main(String[] args) {
+        String code = GoodItemKeyGenerator.generatorKey ( 80516);
+        System.out.println (code);
     }
 }
