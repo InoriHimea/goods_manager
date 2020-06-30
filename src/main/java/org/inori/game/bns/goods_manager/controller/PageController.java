859b063e7c6fc1087fdb60d1d575010502aed105
InoriHimea <icarus347@gmail.com>
2020-06-30 20:35:02 +0800

    过于迷惑222

--- src/main/java/org/inori/game/bns/goods_manager/controller/PageController.java
+++ src/main/java/org/inori/game/bns/goods_manager/controller/PageController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @Slf4j
 @Api(value = "主页", tags = {"页面相关"}, hidden = true)
-public class HomeController {
+public class PageController {
 
     /**
      * 主页跳转，暂为swagger页面
